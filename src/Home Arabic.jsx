3d3a2f88df4ebb7efa71be29127c28d8d191eaa7
Mr. Robot <harikrznan@gmail.com>
2022-04-02 01:27:43 +0530

    Contact Details

--- src/Home Arabic.jsx
+++ src/Home Arabic.jsx
@@ -438,17 +438,21 @@ export default function Arabic() {
                                 اتصل بنا
                             </div>
                             <div className="flex items-center mt-5 justify-end">
-                                <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
-                                <img className="w-10 mx-4 h-10" src="img/whatsapp.png" alt="" />
-                                <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white  " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
+                            </div>
+                            <div className="flex items-center mt-5 justify-end">
+                                <a href="mailto:">
+                                    <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
+
+                                </a>
+                                <a href="https://wa.me/966536393232">
+                                    <img className="w-10 mx-4 h-10" src="img/whatsapp.png" alt="" />
+                                </a>
+                                <a href="tel:+966536393232">
+                                    <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white  " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
+                                </a>
                             </div>
                             <div className="text-right mt-5">
-                                عنوان
-                                <br />
-                                <br />
-
-                                <br />
-
+                                مركز الجناح الأبيض الطبي للاسنان
                             </div>
                         </div>
                     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -432,20 +432,24 @@ export default function Home() {
 
                         <div className="px-10 flex flex-col justify-center items-end">
                             <div className="text-2xl text-right">
-                                Call us
+                                COntact us
                             </div>
                             <div className="flex items-center mt-5 justify-end">
-                                <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white mx-4 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
-                                <img className="w-10 h-10" src="img/whatsapp.png" alt="" />
-                                <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white ml-4 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
+                                <a href="mailto:">
+                                    <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white mx-4 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" /></svg>
+                                </a>
+                                <a href="https://wa.me/966536393232">
+                                    <img className="w-10 h-10" src="img/whatsapp.png" alt="" />
+                                </a>
+                                <a href="tel:+966536393232">
+                                    <svg className="w-10 h-10 rounded-full bg-cyan-500 p-2 text-white ml-4 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" /><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" /></svg>
+                                </a>
                             </div>
-                            <div className="text-right mt-5">
-                                Central Riyadh
-                                <br />
-                                Eastern Ring Road
-                                <br /> between Exit 10 and 11
-                                <br /> Al-Quds District
-                            </div>
+                            <a href="https://goo.gl/maps/9kJUcRbWDM5XMiYDA">
+                                <div className="text-right mt-5">
+                                    White Wing Dental Medical Center
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
